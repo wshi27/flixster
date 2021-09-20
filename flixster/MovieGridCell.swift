f8c46295fa899c25dd72b5cbb1999b47f8836fdc
wshi27 <wshi27@hotmailcom>
2021-09-20 09:18:15 -0400

    added movie detail and superhero tab bar

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Weiwei Shi on 9/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
